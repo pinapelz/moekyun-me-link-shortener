60f815e9e87d4042c709879f8ac5911487fa9cbf
Pinapelz <yukais@pinapelz.com>
2024-02-17 22:52:05 -0800

    refactor: pep8 style

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import string
 import random
 import secrets
 import psycopg2
-from psycopg2 import Error
+import redis
 import os
 
 app = Flask(__name__)
@@ -24,8 +24,18 @@ MOE_IMAGE = "https://files.catbox.moe/bqtys4.webp"
 MOE_QUOTE = "Have a moe day today!"
 CUSTOM_URL_REQUIRE_AUTH = True
 
+
+#######################################
+#          POSTGRES HANDLER           #
+#######################################
+
 class PostgresHandler:
-    def __init__(self, username: str, password: str, host_name: str, port: int, database: str):
+    def __init__(self,
+                 username: str,
+                 password: str,
+                 host_name: str,
+                 port: int,
+                 database: str):
         db_params = {
             "dbname": database,
             "user": username,
@@ -35,14 +45,13 @@ class PostgresHandler:
         }
         self._connection = psycopg2.connect(**db_params)
         print("Handler Success")
-    
 
     def create_table(self, name: str, column: str):
         cursor = self._connection.cursor()
         cursor.execute(f"CREATE TABLE IF NOT EXISTS {name} ({column})")
         self._connection.commit()
         cursor.close()
-    
+
     def check_row_exists(self, table_name: str, column_name: str, value: str):
         cursor = self._connection.cursor()
         query = f"SELECT 1 FROM {table_name} WHERE {column_name} = %s"
@@ -54,23 +63,24 @@ class PostgresHandler:
             return True
         else:
             return False
-    
+
     def insert_row(self, table_name, column, data):
         try:
-            cursor  = self._connection.cursor()
+            cursor = self._connection.cursor()
             placeholders = ', '.join(['%s'] * len(data))
-            query = f"INSERT INTO {table_name} ({column}) VALUES ({placeholders})"
+            query = f"""INSERT INTO {table_name}({column})
+                            VALUES ({placeholders})"""
             cursor.execute(query, data)
             self._connection.commit()
             print("Data Inserted:", data)
-        except Error as err:
+        except psycopg2.Error as err:
             self._connection.rollback()
             print("Error inserting data")
             print(err)
             if "duplicate key" not in str(err).lower():
                 return False
         return True
-    
+
     def get_rows(self, table_name: str, column: str, value: str):
         try:
             cursor = self._connection.cursor()
@@ -78,14 +88,22 @@ class PostgresHandler:
             cursor.execute(query, (value,))
             result = cursor.fetchall()
             return result
-        except Error as e:
+        except psycopg2.Error as e:
             self._connection.rollback()
             print(f"Failed to fetch row from {table_name} WHERE {column} is {value}")
             print(e)
             return False
-    
+
     def close_connection(self):
         self._connection.close()
+
+
+#######################################
+#          REDIS HANDLER           #
+#######################################
+
+class RedisHandler:
+    def __init_
 
 
 def create_database_connection():
@@ -98,7 +116,7 @@ def create_database_connection():
         SITE_URL = os.environ.get("SITE_URL")
         MOE_IMAGE = os.environ.get("MOE_IMAGE")
         MOE_QUOTE = os.environ.get("MOE_QUOTE")
-        
+
     else:
         parser = configparser.ConfigParser()
         parser.read("config.ini")
@@ -108,19 +126,30 @@ def create_database_connection():
         password = CONFIG.get("database", "password")
         database = CONFIG.get("database", "database")
         port = CONFIG.get("database", "port")
-    return PostgresHandler(host_name=hostname, username=user, password=password, database=database, port=5432)
-
+    return PostgresHandler(host_name=hostname,
+                           username=user,
+                           password=password,
+                           database=database,
+                           port=5432)
 
 
 def initialize_database():
     sql_handler = create_database_connection()
     sql_handler.create_table(
         "shortened_links",
-        "id SERIAL PRIMARY KEY, link VARCHAR(255), shortened_link VARCHAR(255) UNIQUE, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, captcha VARCHAR(255)"
+        """
+        id SERIAL PRIMARY KEY,
+        link VARCHAR(255),
+        shortened_link VARCHAR(255) UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        captcha VARCHAR(255)
+        """
     )
     sql_handler.create_table(
         "authentication",
-        "id SERIAL PRIMARY KEY, authkey VARCHAR(255) UNIQUE"
+        """id SERIAL PRIMARY KEY,
+        authkey VARCHAR(255) UNIQUE
+        """"
     )
     sql_handler.close_connection()
 
@@ -130,15 +159,18 @@ def generate_random_hash(length=6):
     random_hash = ''.join(secrets.choice(characters) for _ in range(length))
     return random_hash
 
+
 @app.route('/')
 def main_page():
     bottom_graphic = random.choice(corner_graphics)
     if os.environ.get("CHECK_SETUP_EACH_VISIT") == "True":
-        initialize_database()
+        pass
+        # initialize_database()
     return render_template('index.html',
                            moe_image_url=MOE_IMAGE,
                            moe_quote=MOE_QUOTE,
                            graphic=bottom_graphic)
+
 
 @app.route('/api/add_shortened', methods=['POST'])
 def new_link():
@@ -146,25 +178,27 @@ def new_link():
     requested_link = request.form.get("url")
     captcha = request.form.get("captcha")
     if captcha is None or captcha not in ["VTuber", "None"]:
-        print("Invalid captcha")
         captcha = "None"
     if requested_link is None:
-        print("No link provided")
         return abort(400, "No link provided")
     if requested_link.strip() == "":
         return abort(400, "Cannot shorten empty link")
-    if not requested_link.startswith("http://") and not requested_link.startswith("https://"):
+    if not requested_link.startswith("http://") \
+            and not requested_link.startswith("https://"):
         requested_link = "https://" + requested_link
     hash_value = generate_random_hash()
 
     while True:
-        if server.check_row_exists("shortened_links", "shortened_link", hash_value):
+        if server.check_row_exists("shortened_links", "shortened_link",
+                                   hash_value):
             hash_value = generate_random_hash()
         else:
             break
-    server.insert_row("shortened_links", "link, shortened_link, captcha", (requested_link, hash_value, captcha))
+    server.insert_row("shortened_links", "link, shortened_link, captcha",
+                      (requested_link, hash_value, captcha))
     server.close_connection()
     return jsonify(SITE_URL+"/"+hash_value)
+
 
 @app.route("/api/add_custom", methods=['POST'])
 def add_custom():
@@ -175,16 +209,17 @@ def add_custom():
     password = request.headers.get('X-AUTHENTICATION')
     if password is None and CUSTOM_URL_REQUIRE_AUTH:
         return abort(401, "Invalid Authentication")
-    if not server.check_row_exists("authentication", "authkey", password) and CUSTOM_URL_REQUIRE_AUTH:
+    if not server.check_row_exists("authentication", "authkey", password) \
+            and CUSTOM_URL_REQUIRE_AUTH:
         server.close_connection()
         return abort(401, "Invalid Authentication")
 
     if requested_link is None:
-        print("No link provided")
         return abort(400, "No link provided")
     if requested_link.strip() == "":
         return abort(400, "Cannot shorten empty link")
-    if not requested_link.startswith("http://") and not requested_link.startswith("https://"):
+    if not requested_link.startswith("http://") \
+            and not requested_link.startswith("https://"):
         requested_link = "https://" + requested_link
     if custom_link is None:
         return abort(400, "No custom link provided")
@@ -199,14 +234,14 @@ def add_custom():
     server.close_connection()
     return jsonify(SITE_URL+"/"+custom_link)
 
+
 @app.route('/<path>')
 def expand_url(path):
     server = create_database_connection()
     if server.check_row_exists("shortened_links", "shortened_link", path):
-        print("Link found")
         link = server.get_rows("shortened_links", "shortened_link", path)[0][1]
-        captcha = server.get_rows("shortened_links", "shortened_link", path)[0][4]
-        print("Captcha:", captcha)
+        captcha = server.get_rows("shortened_links",
+                                  "shortened_link", path)[0][4]
         server.close_connection()
         if captcha == "VTuber":
             return render_template("auth.html", redirect_url=link)
@@ -215,9 +250,11 @@ def expand_url(path):
     server.close_connection()
     return abort(404, "Link not found")
 
+
 @app.route("/create/new_auth")
 def new_auth():
     return render_template("auth.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
